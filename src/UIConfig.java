b082113ac5a54bfba790ef83cb75c64457c3cfb9
uhkay <konstantius.kevin@gmail.com>
2025-02-28 10:17:52 +0700

    move UI config

---
+++ src/UIConfig.java
@@ -0,0 +1,9 @@
+import java.awt.Color;
+import java.awt.Font;
+
+public class UIConfig {
+    public static final Color PRIMARY_COLOR = new Color(0, 102, 255);
+    public static final Color SECONDARY_COLOR = new Color(0, 51, 204);
+    public static final Font BUTTON_FONT = new Font("Arial", Font.BOLD, 14);
+    public static final Font TITLE_FONT = new Font("Arial", Font.BOLD, 18);
+}
