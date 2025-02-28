57130e6bc91c1c7901af49d1b5af449c0bc628b6
Ken-jaz <annahar.madura@gmail.com>
2025-02-28 15:07:42 +0700

    Add add panel form

--- src/DashboardForm.java
+++ src/DashboardForm.java
@@ -7,9 +7,13 @@ import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.ObjectInputFilter.Config;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -20,6 +24,9 @@ import java.util.Properties;
 public class DashboardForm extends JFrame {
     private JPanel cardPanel;
     private CardLayout cardLayout;
+    private JTextField addUsernameField;
+    private JTextField addTittleField;
+    private JPasswordField addPasswordField;
 
     // Database configuration constants
     private static Properties properties = new Properties();
@@ -50,6 +57,7 @@ public class DashboardForm extends JFrame {
         cardPanel = new JPanel(cardLayout);
 
         JPanel navPanel = createNavPanel();
+        JPanel addPanel = createaddPanel();
 
         add(navPanel, BorderLayout.SOUTH);
         add(cardPanel, BorderLayout.CENTER);
@@ -76,6 +84,58 @@ public class DashboardForm extends JFrame {
         navPanel.add(addButton);
 
         return navPanel;
+    }
+
+    private JPanel createaddPanel() {
+        JPanel addPanel = new JPanel();
+        addPanel.setLayout(new GridBagLayout());
+        addPanel.setBackground(Color.WHITE);
+        addPanel.setBorder(BorderFactory.createCompoundBorder(
+            BorderFactory.createLineBorder(UIConfig.PRIMARY_COLOR, 2),
+            BorderFactory.createEmptyBorder(15, 15, 15, 15)
+            ));
+        
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+
+        JLabel titLabel = new JLabel("ADD");
+        addTittleField = new JTextField(20);
+        addUsernameField = new JTextField(20);
+        addPasswordField = new JPasswordField(20);
+        JButton addButton = new JButton("add");
+        styleButton(addButton);
+        
+        titLabel.setFont(UIConfig.TITLE_FONT);
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 2;
+        addPanel.add (titLabel, gbc);
+
+        gbc.gridy = 1;
+        gbc.gridwidth = 1;
+        addPanel.add(new JLabel("Tittle: "), gbc);
+        gbc.gridx = 1;
+        addPanel.add(addTittleField, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        addPanel.add(new JLabel("Username: "), gbc);
+        gbc.gridx = 1;
+        addPanel.add(addUsernameField, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        addPanel.add(new JLabel("Password: "), gbc);
+        gbc.gridx = 2;
+        addPanel.add(addPasswordField, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 4;
+        gbc.gridwidth = 1;
+        addPanel.add(addButton, gbc);
+
+        return addPanel;
     }
 
     private void styleButton(JButton button) {
